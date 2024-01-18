7be7c1d363a40cc834eba3d838e173a1ffbf7e1d
Avinash Sahu <96678635+Avinash9694@users.noreply.github.com>
2024-01-18 17:59:24 +0530

    pagination added

--- src/reducer.js
+++ src/reducer.js
@@ -10,6 +10,7 @@ const reducer = (state, action) => {
   switch (action.type) {
     case SET_LOADING:
       return { ...state, isLoading: true };
+
     case SET_STORIES:
       return {
         ...state,
@@ -17,12 +18,32 @@ const reducer = (state, action) => {
         hits: action.payload.hits,
         nbPages: action.payload.nbPages,
       };
+
     case REMOVE_STORY:
       const updatedState = state.hits.filter(
         (item) => item.objectID !== action.payload
       );
       return { ...state, hits: [...updatedState] };
 
+    case HANDLE_SEARCH:
+      return { ...state, query: action.payload, page: 0 };
+
+    case HANDLE_PAGE:
+      if (action.payload === "inc") {
+        let nextPage = state.page + 1;
+        if (nextPage > state.nbPages - 1) {
+          nextPage = 0;
+        }
+        return { ...state, page: nextPage };
+      }
+      if (action.payload === "dec") {
+        let prevPage = state.page - 1;
+        if (prevPage < 0) {
+          prevPage = state.nbPages - 1;
+        }
+        return { ...state, page: prevPage };
+      }
+      break;
     default:
       throw new Error(`No matching "${action.type}" action type`);
   }
